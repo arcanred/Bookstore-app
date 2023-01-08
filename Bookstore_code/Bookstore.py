be957fe9486f37b08da97107dc835f7fd14ae820
Arcan <mcgrathd3254@yahoo.com>
2023-01-08 15:41:20 -0500

    first commit

---
+++ Bookstore_code/Bookstore.py
@@ -0,0 +1,9 @@
+###Bookstore/Book Reccomendation site 
+# Functionality - allows user to search through different Genres and find the top 10 books through a sort and filter method
+#                 filter method can be filtered by rating, author, publication year, publication and searched for directly with title of book
+#                 may add ability to search for prices online for the purchase of recommended book which could be sorted by price
+# Data Needed - author_name, book_title, publishing_date, publication, genre, number_of_pages, price, price_location###
+
+
+
+
