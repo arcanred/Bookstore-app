d0a9b45c6dd7d1252621cec19ca528b850fa9532
Arcan <mcgrathd3254@yahoo.com>
2023-01-08 21:28:32 -0500

    Add data Update focus

---
+++ Bookstore_code/data.py
@@ -0,0 +1,7 @@
+#different genres
+
+genres = ['Romance', 'Action', 'Adventure', 'Horror', 'Comedy']
+
+#Data Needed - genre, author_name, book_name, rating, publication_year, isbn
+
+book_store_data = [['Romance', 'Sarah J. Mass', 'A Court of Thorns and Roses', '4', '2020', '1635575567'], ['Romance', 'Colleen Hoover','Ugly Love: A Novel', '2', '2014', '1476753180']]
